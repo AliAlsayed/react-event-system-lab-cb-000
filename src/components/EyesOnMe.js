d76feca7dcca0e722534239dc3ed8b2f3b77c57f
Ali Alsayed <a_alsayed87@hotmail.com>
2019-11-05 08:34:45 +0000

    Automatically backed up by Learn

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,6 +1,10 @@
 // Code EyesOnMe Component Here
 import React from 'react';
 export default class EyesOnMe extends React.Component {
+
+  hey = () => {
+    console.log('Hey! Eyes on me!');
+  }
   render(){
     return(
       <button type="password" onKeyUp={console.log('Entering password...')}> </button>
